d71cae67d691d260acf21736c69bec64d7f3cab6
Ahmet Emin Akpınar <aeakpinar7@gmail.com>
2022-10-20 22:32:11 +0300

    deneme class güncellendi

--- src/app/deneme-class.ts
+++ src/app/deneme-class.ts
@@ -1,6 +1,13 @@
 export class DenemeClass {
-  baslik!:string;
-  icerik!:string;
-  kategori!:string;
+  constructor(baslik:any,icerik:any,kategori:any,tarih:any) {
+    this.baslik=baslik;
+    this.icerik=icerik;
+    this.kategori=kategori;
+    this.tarih=tarih;
+  }
+  baslik:any='';
+  icerik:any='';
+  kategori:any='';
+  tarih:any='';
 
 }
